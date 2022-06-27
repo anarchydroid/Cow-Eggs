1a2b0f41f72aecfc207c370544df0d2ed582af32
anarchydroid <slaughterheights@gmail.com>
2022-06-27 14:00:45 -0500

    Logic faze 2

--- key.js
+++ key.js
@@ -1,1 +1,34 @@
-console.log("console testing")
+//let key = //fyarray clicked in order
+
+
+
+document.querySelectorAll('.clickable').forEach(el => {
+    el.addEventListener('click', handleLetterClick);
+})
+
+function eggWinner() {
+  let person = prompt("Please enter your name");
+  if (person != null) {
+    document.getElementById("eggOwner").innerHTML =
+    person + " IS THE OWNER OF THIS COW EGG!";
+  }
+}
+
+function handleLetterClick(event) {
+    console.log("letter clicked", event.target.innerHTML);
+    const properOrder = 'fuckyou';
+    if ( !window.clickString ) window.clickString = '';
+    const letterClicked = event.target.innerHTML;
+    const testClickString = window.clickString + letterClicked;
+    if ( properOrder.indexOf(testClickString) === 0 ) {
+        //clicked a letter in the right order
+        window.clickString = testClickString
+    } else {
+        //clicked a letter out of order
+        window.clickString = '';
+    }
+    if ( window.clickString === properOrder ) {
+        //all letters properly clicked in the right order
+        eggWinner();
+    }
+}
